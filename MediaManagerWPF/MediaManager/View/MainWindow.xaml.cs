1705f21ec6d86269773a9791e9739dedd1a61bb3
Jonathan Fraimorice <yonifra@gmail.com>
2015-11-03 22:55:57 +0200

    "Enter" key in the textbox will now initiate a search

--- MediaManagerWPF/MediaManager/View/MainWindow.xaml.cs
+++ MediaManagerWPF/MediaManager/View/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
-using MahApps.Metro.Controls;
+using System.Windows.Controls;
+using System.Windows.Input;
 using MediaManager.ViewModel;
 
 namespace MediaManager
@@ -7,12 +8,34 @@ namespace MediaManager
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
             DataContext = new MovieInfoViewModel();
         }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // if user didn't press Enter, do nothing
+            if (!e.Key.Equals(Key.Enter)) return;
+
+            // execute the command, if it exists
+            var cmd = GetTextBoxPressEnterCommand((TextBox)sender);
+            cmd?.Execute(null);
+        }
+
+        // declare an attached dependency property
+        public static ICommand GetTextBoxPressEnterCommand(TextBox obj)
+        {
+            return (ICommand)obj.GetValue(TextBoxPressEnterCommandProperty);
+        }
+        public static void SetTextBoxPressEnterCommand(TextBox obj, ICommand value)
+        {
+            obj.SetValue(TextBoxPressEnterCommandProperty, value);
+        }
+        public static readonly DependencyProperty TextBoxPressEnterCommandProperty =
+            DependencyProperty.RegisterAttached("TextBoxPressEnterCommand", typeof(ICommand), typeof(MainWindow));
     }
 }
